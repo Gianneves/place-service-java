33ec0a0ba830c91ed9f7da6537ad1a2af892804b
Gian Neves <gianneves@hotmail.com>
2023-10-30 14:46:17 -0300

    feat: Create Place, PlaceRepository, PlaceService

---
+++ src/main/java/com/gianneves/placeservice/domain/Place.java
@@ -0,0 +1,8 @@
+package com.gianneves.placeservice.domain;
+
+import java.time.LocalDateTime;
+
+public record Place(
+        Long id, String name, String slug, String state, LocalDateTime createdAtt, LocalDateTime updatedAt) {
+
+}
---
+++ src/main/java/com/gianneves/placeservice/domain/PlaceService.java
@@ -0,0 +1,13 @@
+package com.gianneves.placeservice.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Mono;
+
+public class PlaceService {
+    @Autowired
+    private PlaceRepository placeRepository;
+
+    public Mono<Place> create(Place place) {
+        return placeRepository.save(place);
+    }
+}
